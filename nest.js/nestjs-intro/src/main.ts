45f7ec86d16c60df5a679b4ccd3b7332d66f50b3
Garunyaa <garunyaa@sparkouttech.com>
2025-01-02 17:30:28 +0530

    books module CRUD added

--- nest.js/nestjs-intro/src/main.ts
+++ nest.js/nestjs-intro/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 
-async function bootstrap() {
+const port = process.env.PORT || 3000;
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const firstError = errors[0];
+        const errorMessage = Object.values(firstError.constraints)[0];
+        return new BadRequestException({
+          status: false,
+          status_code: 400,
+          message: errorMessage,
+          data: {},
+        });
+      },
+    })
+  );
+
+  await app.listen(port, () => {
+    console.log(`Application listening on port ${port}`);
+  });
 }
 bootstrap();
